c1b91ed4cfb4f09083c197c36aba59d94d0febc5
Tanvi <145559687+TanviS3000@users.noreply.github.com>
2023-11-03 17:46:43 +0530

    Update Quiz.kt

--- Quiz.kt
+++ Quiz.kt
@@ -8,7 +8,12 @@ data class Question(
 val questions = listOf(
     Question("What is the capital of France?", listOf("1. London", "2. Berlin", "3. Paris", "4. Madrid"), 3),
     Question("Which planet is known as the Red Planet?", listOf("1. Earth", "2. Mars", "3. Venus", "4. Jupiter"), 2),
-    // Add more questions here
+    Question("Which is the deepest point in ocean?", listOf("1. Mariana trench", "2. Puerto Rico trench", "3. Philippine trench", "4. Tonga trench"), 1),
+    Question("Which is the longest river in the world?", listOf("1. Amazon", "2. Nile", "3. Great Ganga", "4. Yangatse"), 2),
+    Question("Which is the largest animal in the world?", listOf("1. Giraffe", "2. Tiger", "3. Elephant", "4. Blue Whale"), 4),
+    Question("Which is the biggest continent in the world?", listOf("1. Asia", "2. North America", "3. Africa", "4. Antarctica"), 1),
+    Question("Which is India’s first super computer?", listOf("1. Param8", "2. Param80", "3. Param800", "4. Param8000"), 4),
+    Question("Which is largest island in the world?", listOf("1. New Guinea", "2. Hawaii", "3. Greenland", "4. Andaman Nicobar"), 3)
 )
 
 var score = 0
