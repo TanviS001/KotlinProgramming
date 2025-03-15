0b55db39976ca0c822a73717734756bb1077bc7c
Tanvi <145559687+TanviS001@users.noreply.github.com>
2025-03-15 20:44:03 +0530

    Create String.kt

---
+++ String.kt
@@ -0,0 +1,12 @@
+fun main(args:Array<String>) {
+    val a: String = "Hello"
+    val b: String = "My name is Android"
+    val c: String = "             sjdb          "
+    println(a.length)
+    println(a.uppercase())
+    println(a.lowercase())
+    println(a.equals(b))
+    println(a.plus(b))
+    println(a.isEmpty())
+    println(c.trim())
+}
